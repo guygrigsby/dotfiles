61665d0bd8845a67a4770debb7c4015ab55b22a1
Guy J Grigsby <guy.grigsby@nytimes.com>
2021-01-25 09:03:45 -0700

    add scripts

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 #
-export PATH=/usr/local/bin:$PATH:$SCRIPTS:$GOPATH/bin:/usr/local/go/bin:$HOME/lib
+export PATH=/usr/local/bin:$HOME/scripts:$GOPATH/bin:/usr/local/go/bin:$HOME/lib:$PATH
 export PATH=${0:A:h}/bin:$PATH
-export PATH=/Users/guy/scripts:$PATH
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
