cf3d713ded9e010919aa822db44e87236c4d0842
Guy J Grigsby <guy.grigsby@hashicorp.com>
2023-04-21 13:21:32 -0600

    consolidate cvars

--- zsh/vars.zsh
+++ zsh/vars.zsh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 # dev
-#export BOUNDARY_ADDR="https://5b489ba2-97ce-42d6-80dd-af3d17e401a5.boundary.hcp.dev"
 export BOUNDARY_ADDR="http://localhost:9200"
 export GOLANG_PROTOBUF_REGISTRATION_CONFLICT=warn
 export ICLOUD_DIR=$HOME/Library/Mobile\ Documents/com~apple~CloudDocs
@@ -12,3 +11,5 @@ export HC=$HOME/go/src/github.com/hashicorp
 # use 1password ssh agent. This allows direct access to ssh keys stored in 1password private vault.
 export SSH_AUTH_SOCK=~/.1password/agent.sock
 export HOMEBREW_NO_AUTO_UPDATE=1
+export NOMAD_TOKEN_NAME="cloud-boundary"
+export CONSUL_HTTP_TOKEN_NAME="cloud-team"
